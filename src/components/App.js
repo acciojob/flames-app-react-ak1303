872a9fa34fc46e33723f93a7443b962a3f0fdb77
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-04-17 19:15:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,39 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+
+    const flames = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"];
+
+  const [name1, setName1] = useState("");
+  const [name2, setName2] = useState("");
+  const [ans,setAns] =useState("");
+
+    const getAnswer = () =>{
+        const map = new Map();
+        for(let i = 0; i <name1.length; i++){
+            if(map.has(name1[i])){
+                map.set(name1[i],map.get(name1[i])+1);
+            }else map.set(name1[i],1);
+        }
+        let count=0;
+        for(let i = 0; i <name2.length; i++){
+            if(map.has(name2[i]) && map.get(name2[i])>0){
+                count++;
+                map.set(name2[i],map.get(name2[i])-1);
+            }
+        }
+        let result = flames[(name1.length+name2.length-(count*2))%6];
+        setAns(result);
+    }
     render() {
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+               <input type="text" value={name1} onChange={(e)=>setName1(e.target.value)}/>
+                <input type="text" value={name2} onChange={(e)=>setName2(e.target.value)}/>
+                <button onClick={getAnswer}>Calculate Relationship Future</button>
+                <button onClick={()=>setAns("")}>Clear</button>
+                {ans && <div>{ans}</div>}
             </div>
         )
     }
